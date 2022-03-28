f904f92ff8e4ad16ac8405e5f450fac468bb4068
moein9gh <moein9gh@gmail.com>
2022-03-28 16:14:40 +0430

    add git ignore

--- compiler.go
+++ compiler.go
@@ -16,16 +16,25 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
+var incomingPayload map[string]interface{}
+
+type QuestionPayload struct {
+	questionNumber string `json:"questionNumber"`
+	byteArray      []byte `json:"byteArray"`
+	fileSize       int64  `json:"size"`
+	name           string
 }
 
 type Test struct {
 	Id     int    `json:"id"`
 	Input  string `json:"input"`
 	Output string `json:"output"`
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
 
 func (qp *QuestionPayload) runTests(tests []Test) []bool {
@@ -62,32 +71,10 @@ func (qp *QuestionPayload) runTests(tests []Test) []bool {
 
 }
 
-var incomingPayload map[string]interface{}
-
-type QuestionPayload struct {
-	questionNumber string `json:"questionNumber"`
-	byteArray      []byte `json:"byteArray"`
-	fileSize       int64  `json:"size"`
-	name           string
-}
-
 func (qp *QuestionPayload) create(questionNumber string, byteArray []byte, fileSize int64) {
 	qp.fileSize = fileSize
 	qp.byteArray = byteArray
 	qp.questionNumber = questionNumber
-}
-
-func getExamplesOfQuestion(questionNumber, questions string) []Test {
-	question := gjson.Get(questions, questionNumber+".examples")
-
-	var examples []Test
-
-	err := json.Unmarshal([]byte(question.String()), &examples)
-
-	failOnError(err, "failed to unmarshal")
-
-	return examples
-
 }
 
 func (qp *QuestionPayload) startTest(questions string) (results []bool) {
@@ -103,10 +90,22 @@ func (qp *QuestionPayload) createFile() {
 	err := os.WriteFile(path+".cpp", qp.byteArray, 0644)
 	failOnError(err, "failed to create file")
 
-	cmd, err := exec.Command("g++", path+".cpp", "-o", path+".exe").Output()
+	_, err = exec.Command("g++", path+".cpp", "-o", path+".exe").Output()
 	failOnError(err, "failed to build cpp file")
 
-	fmt.Println(cmd)
+}
+
+func getExamplesOfQuestion(questionNumber, questions string) []Test {
+	question := gjson.Get(questions, questionNumber+".examples")
+
+	var examples []Test
+
+	err := json.Unmarshal([]byte(question.String()), &examples)
+
+	failOnError(err, "failed to unmarshal")
+
+	return examples
+
 }
 
 func connectAndListenToRabbit(messageReceived chan string, questions string) {
@@ -118,15 +117,56 @@ func connectAndListenToRabbit(messageReceived chan string, questions string) {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	err = ch.ExchangeDeclare(
+		"messages", // name
+		"topic",    // type
+		true,       // durable
+		false,      // auto-deleted
+		false,      // internal
+		false,      // no-wait
+		nil,        // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
 	queue, err := ch.QueueDeclare(
-		"submits", // name
-		false,     // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		nil,       // arguments
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
+
+	//err = ch.ExchangeDeclare(
+	//	"messages", // name
+	//	"topic",    // type
+	//	true,       // durable
+	//	false,      // auto-deleted
+	//	false,      // internal
+	//	false,      // no-wait
+	//	nil,        // arguments
+	//)
+	//failOnError(err, "Failed to declare an exchange")
+	//
+	//err = ch.Publish(
+	//	"messages", // exchange
+	//	"#",        // routing key
+	//	false,      // mandatory
+	//	false,      // immediate
+	//	amqp.Publishing{
+	//		ContentType: "text/plain",
+	//		Body:        []byte("hi"),
+	//	})
+	//
+	//failOnError(err, "Failed to publish a message")
+
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		"#",        // routing key
+		"messages", // exchange
+		false,
+		nil)
 
 	msgs, err := ch.Consume(
 		queue.Name, // queue
@@ -145,7 +185,9 @@ func connectAndListenToRabbit(messageReceived chan string, questions string) {
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message")
+			log.Println("Received a message")
+			err := d.Ack(false)
+			failOnError(err, "failed to ack")
 			messageReceived <- fmt.Sprintf("%s", d.Body)
 		}
 	}()
@@ -159,8 +201,7 @@ func receiveMessage(ch chan string, questions string) {
 		for data := range ch {
 
 			jsonBody := fmt.Sprintf("%s", data)
-
-			value := gjson.GetMany(jsonBody, "data.byteArray", "data.size", "data.questionNumber")
+			value := gjson.GetMany(jsonBody, "byteArray", "size", "questionNumber")
 
 			var ba = []byte{}
 			for _, v := range value[0].Array() {
